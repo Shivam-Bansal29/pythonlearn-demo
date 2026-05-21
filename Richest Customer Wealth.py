fad33d5dfffe9cace744930884a9233d842c8c3a
Shivam Bansal <sshivambansal29@gmail.com>
2026-05-21 16:55:52 +0530

    Richest Customer Wealth

---
+++ Richest Customer Wealth.py
@@ -0,0 +1,18 @@
+# Richest Customer Wealth
+account=[[100,5778,10],[7576,8749,94],[685,3487,43299],[47437,3782,3]]
+Money=[]
+for i in account:
+    wealth=0
+    for j in i:
+        wealth=wealth+j
+    print(wealth)
+    Money.append(wealth)
+Max=Money[0]
+for x in range(0,len(Money)):
+    if(Money[x]>Max):
+        Max=Money[x]
+        cos=x+1
+print(f"{cos} person has has max wealth {Max}")
+    
+               
+                 
