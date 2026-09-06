8e622593078bc809b326802e0e4ddc96c9a78e57
Shivam Bansal <sshivambansal29@gmail.com>
2026-09-06 18:30:35 +0530

    Best time to buy and sell stock leetcode problem soluton

---
+++ Best_Time_to_Buy_Sell_Stock.py
@@ -0,0 +1,17 @@
+# Best Time to Buy and Sell Stock
+prices =[1,2]
+purchase = prices[0]
+profit = 0 
+for i in range(1,len(prices)):
+        print(85)
+        if(prices[i]<purchase):
+                purchase = prices[i]
+        
+        cprofit = prices[i+1] - purchase
+        
+        if(cprofit > profit):
+                profit = cprofit
+        
+print(prices)
+print(profit)
+
